2d8c9f7a123a15ec52f7ebbe9b7a69052586faaf
Yuji Yamaguchi <YuG1224@users.noreply.github.com>
2016-05-09 17:43:14 +0900

    src追加

---
+++ src/clipeace.js
@@ -0,0 +1,32 @@
+const Promise = require('bluebird');
+
+const clipeace = function(elem) {
+  return new Promise((resolve, reject) => {
+
+    const selection = window.getSelection();
+    const range = document.createRange();
+    let text = undefined;
+    let copy = false;
+
+    range.selectNodeContents(elem);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    text = selection.toString();
+    copy = document.execCommand('copy');
+    selection.removeAllRanges();
+
+    const res = {
+      text: text,
+      copy: copy
+    };
+
+    if (copy) {
+      resolve(res);
+    } else {
+      reject(res);
+    }
+
+  });
+};
+
+module.exports = clipeace;
